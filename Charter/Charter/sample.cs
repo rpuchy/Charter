65f21e749f12cbb156b0bc941b4a12d0aa0678b8
Rudi Puchy <Rudi Puchy>
2017-09-18 21:23:35 +1000

    first commit

---
+++ Charter/Charter/sample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace Charter
+{
+    public partial class StackedAreaExample : UserControl
+    {
+        public StackedAreaExample()
+        {
+
+
+            
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public Func<double, string> XFormatter { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+    }
+}
